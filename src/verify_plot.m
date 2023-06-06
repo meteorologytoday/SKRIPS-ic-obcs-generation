b67b2abc59b2bb9c5a341d8f66077e513f5c7665
t2hsu <t2hsu@ring.ucsd.edu>
2023-06-06 09:28:28 -0700

    go

--- src/verify_plot.m
+++ src/verify_plot.m
@@ -6,15 +6,22 @@ addpath('./mitgcm-preprocess/gen_ic_obcs');
 varname = 'S';
 cbrng = [33, 35];
 cmap = 'redblue';
-input_dir = '/home/t2hsu/projects/SKRIPS-case-generation/sample/output_final';
+input_dir = '/home/t2hsu/verify';
 
-date_str='2018-01-21_00';
+date_str='2017-02-01_00';
+obcs_skips = 0
+%ic_filename = sprintf('%s/initial_conditions/hycom_%s_%s.bin', input_dir, varname, date_str);
+%bnd_east_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_east_%s.bin', input_dir, varname, date_str);
+%bnd_west_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_west_%s.bin', input_dir, varname, date_str);
+%bnd_north_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_north_%s.bin', input_dir, varname, date_str);
+%bnd_south_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_south_%s.bin', input_dir, varname, date_str);
 
-ic_filename = sprintf('%s/initial_conditions/hycom_%s_%s.bin', input_dir, varname, date_str);
-bnd_east_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_east_%s.bin', input_dir, varname, date_str);
-bnd_west_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_west_%s.bin', input_dir, varname, date_str);
-bnd_north_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_north_%s.bin', input_dir, varname, date_str);
-bnd_south_filename = sprintf('%s/open_boundary_conditions/Rs_%s_obcs_south_%s.bin', input_dir, varname, date_str);
+ic_filename = sprintf('%s/hycom_%s_%s.bin', input_dir, varname, date_str);
+bnd_east_filename = sprintf('%s/obcs_%s_east.bin', input_dir, varname);
+bnd_west_filename = sprintf('%s/obcs_%s_west.bin', input_dir, varname);
+bnd_north_filename = sprintf('%s/obcs_%s_north.bin', input_dir, varname);
+bnd_south_filename = sprintf('%s/obcs_%s_south.bin', input_dir, varname);
+
 
 fprintf('ic_filename = %s\n', ic_filename);
 fprintf('bnd_east_filename = %s\n', bnd_east_filename);
@@ -23,8 +30,8 @@ fprintf('bnd_north_filename = %s\n', bnd_north_filename);
 fprintf('bnd_south_filename = %s\n', bnd_south_filename);
 
 magnify = 1000;
-nx = 40;
-ny = 20;
+nx = 800;
+ny = 400;
 nz = 50;
 
 fmt = 'real*4';
@@ -32,10 +39,11 @@ Ieee = 'b';
 
 ic = rdslice(ic_filename, [nx, ny, nz], 1, fmt, Ieee);
 
-bnd_east  = rdslice(bnd_east_filename,  [ny, nz], 1, fmt, Ieee);
-bnd_west  = rdslice(bnd_west_filename,  [ny, nz], 1, fmt, Ieee);
-bnd_north = rdslice(bnd_north_filename, [nx, nz], 1, fmt, Ieee);
-bnd_south = rdslice(bnd_south_filename, [nx, nz], 1, fmt, Ieee);
+rec = 1 + obcs_skips
+bnd_east  = rdslice(bnd_east_filename,  [ny, nz], rec, fmt, Ieee);
+bnd_west  = rdslice(bnd_west_filename,  [ny, nz], rec, fmt, Ieee);
+bnd_north = rdslice(bnd_north_filename, [nx, nz], rec, fmt, Ieee);
+bnd_south = rdslice(bnd_south_filename, [nx, nz], rec, fmt, Ieee);
 
 figure;
 ax = subplot(1, 1, 1);
